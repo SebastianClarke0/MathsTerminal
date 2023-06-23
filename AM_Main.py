14de87dd89c1735eafdae65388a38a942c8007f7
Sebastian Clarke <77592947+SebastianClarke0@users.noreply.github.com>
2023-06-23 17:56:58 +0100

    Update AM_Main.py

--- AM_Main.py
+++ AM_Main.py
@@ -35,22 +35,26 @@ class App(customtkinter.CTk):
         customtkinter.set_appearance_mode("dark")
         self.grid_rowconfigure(0, weight=1)
 
+        #Topics Scroll
         topics = ["value 1", "value 2", "value 3", "value 4", "value 5", "value 6"]
         self.scrollable_checkbox_frame = MyScrollableFrame(self, title="Topics", topics=topics, width=300, height=728)
         self.scrollable_checkbox_frame.grid(row=0, column=0, padx=10, pady=20)
         
+        #Topics Button
         self.button = customtkinter.CTkButton(self, text="Select Random", command=self.button_callback)
         self.button.grid(row=3, column=0, padx=10, pady=10, sticky="ew")
 
-        self.textbox = customtkinter.CTkTextbox(master=self, width=710, height=200,corner_radius=0)
-        self.textbox.grid(row=0, column=1,padx=10, pady=20, sticky="n")
+        #Text Box
+        self.textbox = customtkinter.CTkTextbox(master=self, width=200, height=200,corner_radius=0)
+        self.textbox.grid(row=0, column=1,padx=10, pady=20, sticky="nw")
         self.textbox.insert("0.0", "Some example text!\n" * 10)
 
+        #
         self.my_frame = MyFrame(master=self, width=710, height=420)
         self.my_frame.grid(row=0, column=1, padx=20, pady=20, sticky="s")
 
         self.button_1 = customtkinter.CTkButton(self, text="open toplevel", command=self.open_toplevel)
-        self.button_1.pack(side="top", padx=20, pady=20)
+        self.button_1.grid(row=0, column=1, padx=20, pady=20, sticky="n")
 
         self.toplevel_window = None
 
