ea8815910556cb8809d1f09bad6393a1dc4861d9
DNAmaster10 <louishollobon@outlook.com>
2023-07-19 14:14:11 +0100

    Update

--- upload.py
+++ upload.py
@@ -1,4 +1,4 @@
-from tkinter import *
+"""from tkinter import *
 from tkinter import ttk
 
 root = Tk()
@@ -6,4 +6,29 @@ frame = ttk.Frame(root, padding=10)
 frame.grid()
 ttk.Label(frame, text="Test").grid(column = 0, row = 0)
 ttk.Button(frame, text="Quit", command=root.destroy).grid(column = 1, row = 0)
-root.mainloop()
+root.mainloop()"""
+
+from dbh import conn, cur
+
+def register_topic():
+    #Get current topics
+    statement = "SELECT topic_name FROM Topics ORDER BY topic_name ASC"
+    data = ()
+    cur.execute(statement, data)
+    topics = cur.fetchall()
+    print ("Total topics: " + str(len(topics)))
+    for row in topics:
+        print(row[0])
+    conn.commit()
+
+while (True):
+    choice = input("1 -> Register topic\n2 -> Register Question\n3 -> Add Answer(s)\n4 -> Exit\n-$ ")
+    match choice:
+        case "1":
+            register_topic()
+        case "2":
+            register_question()
+        case "3":
+            register_question()
+        case "4":
+            print("Please enter a valid input")
